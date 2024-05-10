f9ab5f9194f3798fbe265c61ba25101118eb3d68
AYAN <156874960+Ayanaanonaeem@users.noreply.github.com>
2024-05-11 02:30:38 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+let head=document.getElementById("head");
+let btn=document.getElementById("btn");
+let cont=document.getElementById("koko");
+
+function happy() {
+    cont.style.visibility="visible";
+    
+}
